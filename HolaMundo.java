12270b81aff1cf3988ff6d6895f4b3cd3568913a
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2022-09-30 17:02:22 +0200

    3 canvio

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,7 +4,7 @@ class holaMundo
     // Imprime "Hola Mundo" a la ventana de la terminal.
     public static void main(String args[])
     {
-        System.out.println("HOLA MUnd@, como estas?");
-        System.out.println("Yo estoy muy bien");
+        System.out.println("HOLA MUnd@, c");
+        System.out.println("Yo estoy ");
     }
 }
